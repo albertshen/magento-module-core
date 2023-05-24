782a956d7c2bdb4f5da8290d6951fbd5e8ffb480
Albert Shen <albertshen1206@gmail.com>
2023-05-24 14:57:49 +0800

    up

--- Model/Cache.php
+++ Model/Cache.php
@@ -18,24 +18,8 @@ class Cache extends Psr16Cache
      */
     protected $adapter;
 
-    protected $sss = 225556663;
-
     public function __construct(AdapterInterface $adapter)
-    {//var_dump(get_class_methods($adapter));
- 
-  $computedValue = 'foobar2';
-  $that = $this;
-$value = $adapter->get('my_cache_key4', function ( $item) {
-    $item->expiresAfter(3600);
-
-    // ... do some HTTP request or heavy computations
-
-    $this->sss;
-    return $this->sss;
-});
-
-echo $value; // 'foobar'
-exit;
+    {
         parent::__construct($adapter);
     }
 }
